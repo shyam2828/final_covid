a84765ce89f5803623b2cb36a94151ed73abf39d
shyam2828 <baddogs81@gmail.com>
2020-07-24 17:12:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/testing_covid/UserDetails/Profile.java
@@ -0,0 +1,45 @@
+package com.example.testing_covid.UserDetails;
+
+public class Profile {
+    private String username;
+    private String email;
+    private String status;
+    private String user_id;
+
+    public Profile(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+}
+
